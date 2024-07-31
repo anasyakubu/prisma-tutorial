a31bb03ac61ef3d3025beb05a1263585337c60e3
anasyakubu <yakubuanas04@gmail.com>
2024-07-31 18:17:21 +0100

    Add : Initial Config

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookiesParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+// console.log("Hello World");
